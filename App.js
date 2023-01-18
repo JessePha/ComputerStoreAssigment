b57620d827bd92227bc369863e95acd3d3e17a99
Tommy Pham <60743602+tommy-ph@users.noreply.github.com>
2023-01-18 22:25:14 +0100

    CommentUpdated

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-let bankBalance = 500;
-let outstandingLoan = 0;
-let pay = 0;
-let loanTaken = false;
+let bankBalance = 500; //initialize bank balance
+let outstandingLoan = 0; //initialize outstanding loan
+let pay = 0; //initialize pay variable 
+let loanTaken = false; //initialize loan taken variable
 
+//select element with id "laptop-title"
 const laptopName = document.getElementById("laptop-title");
 
+//update element with id "bank-balance" with bank balance
 document.getElementById("bank-balance").innerHTML =
   "Bank Balance: " + bankBalance;
+//update element with id "outstanding-loan" with outstanding loan
 document.getElementById("outstanding-loan").innerHTML =
   "Outstanding Loan: " + outstandingLoan;
 
+//event listener for button with id "get-loan-button"
+//check if loan has been taken before
 document
   .getElementById("get-loan-button")
   .addEventListener("click", function () {
@@ -19,20 +24,24 @@ document
       );
       return;
     }
+  //prompt user for loan amount
     let loanAmount = prompt("Enter loan amount:");
+  //check if loan amount is more than double of bank balance
     if (loanAmount > bankBalance * 2) {
       alert("You cannot get a loan more than double of your bank balance");
       return;
     }
+    //check if loan amount is valid
     if (loanAmount <= 0) {
       alert("Invalid loan amount");
       return;
     }
-    loanTaken = true;
+    loanTaken = true; //set loan taken to true
     outstandingLoan = loanAmount;
+    //update outstanding loan on webpage
     document.getElementById("outstanding-loan").innerHTML =
       "Outstanding Loan: " + outstandingLoan;
-    document.getElementById("repay-loan-button").style.display = "block";
+    document.getElementById("repay-loan-button").style.display = "block"; //display repay loan button
   });
 
 // document.getElementById("repay-loan-button").addEventListener("click", function() {
@@ -48,38 +57,48 @@ document
 //   document.getElementById("repay-loan-button").style.display = "none";
 // });
 
+//update element with id "pay" with pay
 document.getElementById("pay").innerHTML = "Pay: " + pay;
 
+//event listener for button with id "work-button"
 document.getElementById("work-button").addEventListener("click", function () {
-  pay += 100;
+  pay += 100; //increment pay by 100
   document.getElementById("pay").innerHTML = "Pay: " + pay;
+  //update pay on webpage
 });
 
+//event listener for button with id "bank-button"
+ //check if loan has been taken
 document.getElementById("bank-button").addEventListener("click", function () {
   if (loanTaken) {
-    let loanPayment = pay * 0.1;
-    pay -= loanPayment;
-    outstandingLoan -= loanPayment;
+    let loanPayment = pay * 0.1; //calculate loan payment
+    pay -= loanPayment; //subtract loan payment from pay
+    outstandingLoan -= loanPayment; //subtract loan payment from outstanding loan
     document.getElementById("outstanding-loan").innerHTML =
-      "Outstanding Loan: " + outstandingLoan;
+      "Outstanding Loan: " + outstandingLoan; //update outstanding loan on webpage
   }
-  bankBalance += pay;
-  pay = 0;
-  document.getElementById("pay").innerHTML = "Pay: " + pay;
+  bankBalance += pay; //add pay to bank balance
+  pay = 0; //reset pay to 0
+  document.getElementById("pay").innerHTML = "Pay: " + pay; //update pay on webpage
   document.getElementById("bank-balance").innerHTML =
-    "Bank Balance: " + bankBalance;
+    "Bank Balance: " + bankBalance; //update bank balance on webpage
 });
 
+//event listener for button with id "repay-loan-button"
+//check if pay is enough to repay loan
 document
   .getElementById("repay-loan-button")
   .addEventListener("click", function () {
-    if (pay >= outstandingLoan) {
-      pay -= outstandingLoan;
-      outstandingLoan = 0;
-      loanTaken = false;
+    if (pay >= outstandingLoan) { 
+      pay -= outstandingLoan; //subtract outstanding loan from pay
+      outstandingLoan = 0; //set outstanding loan to 0
+      loanTaken = false; //set loan taken to false
+      //update pay on webpage
       document.getElementById("pay").innerHTML = "Pay: " + pay;
+      //update outstanding loan on webpage
       document.getElementById("outstanding-loan").innerHTML =
         "Outstanding Loan: " + outstandingLoan;
+     //hide repay loan button
       document.getElementById("repay-loan-button").style.display = "none";
     } else {
       alert("You do not have enough pay to repay the loan");
